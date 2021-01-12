2698c34d86cb31160bcefa57456c7b8e3653f201
dexter1918 <sksalmanhaider@outlook.com>
2021-01-12 16:38:16 +0530

    Endpoint handlers moved out of the main() function. Redundant var 'key' is removed. Value of 'Provider' is hardcoded as 'twitter'

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"sort"
 
 	"github.com/gorilla/pat"
 	"github.com/gorilla/sessions"
@@ -14,68 +13,71 @@ import (
 	"github.com/markbates/goth/providers/twitter"
 )
 
-// ProviderIndex is ...
-type ProviderIndex struct {
-	Providers    []string
-	ProvidersMap map[string]string
+// CallbackHandler is the 1st endpoint
+func CallbackHandler(res http.ResponseWriter, req *http.Request) {
+
+	user, err := gothic.CompleteUserAuth(res, req)
+	if err != nil {
+		fmt.Fprintln(res, err)
+		return
+	}
+	t, _ := template.ParseFiles("templates/success.html")
+	t.Execute(res, user)
+}
+
+// LogoutHandler is the 2nd endpoint
+func LogoutHandler(res http.ResponseWriter, req *http.Request) {
+	gothic.Logout(res, req)
+	res.Header().Set("Location", "/")
+	res.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+// AutoAuthenticateHanlder is the 3rd endpoint
+func AutoAuthenticateHanlder(res http.ResponseWriter, req *http.Request) {
+	// This tries to get the user without re-authenticating
+	if gothUser, err := gothic.CompleteUserAuth(res, req); err == nil {
+		t, _ := template.ParseFiles("templates/success.html")
+		t.Execute(res, gothUser)
+	} else {
+		gothic.BeginAuthHandler(res, req)
+	}
+}
+
+// RootHandler is ...
+func RootHandler(res http.ResponseWriter, req *http.Request) {
+	m := make(map[string]string)
+	m["twitter"] = "Twitter"
+
+	providerIndex := &ProviderIndex{
+		Providers:    "twitter",
+		ProvidersMap: m,
+	}
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(res, providerIndex)
 }
 
 func main() {
 	goth.UseProviders(
 		twitter.New("78KQ0Abr9LVvMRoJhF3952pqS", "VjHpiYvFWS6ntGDGmIHR8aIRRLE4kxHDKhj0oDF9bNBU4rF983", "http://127.0.0.1:3000/auth/twitter/callback"),
 	)
-
 	gothic.Store = sessions.NewCookieStore([]byte("VjHpiYvFWS6ntGDGmIHR8aIRRLE4kxHDKhj0oDF9bNBU4rF983"))
-
-	m := make(map[string]string)
-	m["twitter"] = "Twitter"
-
-	var keys []string
-	for k := range m {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-
-	providerIndex := &ProviderIndex{
-		Providers:    keys,
-		ProvidersMap: m,
-	}
 
 	p := pat.New()
 
-	p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
+	p.Get("/auth/{provider}/callback", CallbackHandler)
 
-		user, err := gothic.CompleteUserAuth(res, req)
-		if err != nil {
-			fmt.Fprintln(res, err)
-			return
-		}
-		t, _ := template.ParseFiles("templates/success.html")
-		log.Println("I ran!!")
-		t.Execute(res, user)
-	})
+	p.Get("/logout/{provider}", LogoutHandler)
 
-	p.Get("/logout/{provider}", func(res http.ResponseWriter, req *http.Request) {
-		gothic.Logout(res, req)
-		res.Header().Set("Location", "/")
-		res.WriteHeader(http.StatusTemporaryRedirect)
-	})
+	p.Get("/auth/{provider}", AutoAuthenticateHanlder)
 
-	p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
-		// try to get the user without re-authenticating
-		if gothUser, err := gothic.CompleteUserAuth(res, req); err == nil {
-			log.Println("He ran!!")
-			t, _ := template.ParseFiles("templates/success.html")
-			t.Execute(res, gothUser)
-		} else {
-			gothic.BeginAuthHandler(res, req)
-		}
-	})
+	p.Get("/", RootHandler)
 
-	p.Get("/", func(res http.ResponseWriter, req *http.Request) {
-		t, _ := template.ParseFiles("templates/index.html")
-		t.Execute(res, providerIndex)
-	})
 	log.Println("listening on localhost:3000")
 	log.Fatal(http.ListenAndServe(":3000", p))
+}
+
+// ProviderIndex is ...
+type ProviderIndex struct {
+	Providers    string
+	ProvidersMap map[string]string
 }
