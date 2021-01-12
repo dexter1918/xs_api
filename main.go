a8e64b70ca2502cbb46a7a90eb4321824b25966d
dexter1918 <sksalmanhaider@outlook.com>
2021-01-13 00:55:21 +0530

    MongoDB connected : Insertion of data in  DB is working!

--- main.go
+++ main.go
@@ -1,64 +1,54 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/pat"
 	"github.com/gorilla/sessions"
 	"github.com/markbates/goth"
 	"github.com/markbates/goth/gothic"
 	"github.com/markbates/goth/providers/twitter"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// CallbackHandler is the 1st endpoint
-func CallbackHandler(res http.ResponseWriter, req *http.Request) {
-
-	user, err := gothic.CompleteUserAuth(res, req)
-	if err != nil {
-		fmt.Fprintln(res, err)
-		return
-	}
-	t, _ := template.ParseFiles("templates/success.html")
-	t.Execute(res, user)
+// UserData is ...
+type UserData struct {
+	UserID string
+	Email  string
+	Name   string
 }
 
-// LogoutHandler is the 2nd endpoint
-func LogoutHandler(res http.ResponseWriter, req *http.Request) {
-	gothic.Logout(res, req)
-	res.Header().Set("Location", "/")
-	res.WriteHeader(http.StatusTemporaryRedirect)
-}
-
-// AutoAuthenticateHanlder is the 3rd endpoint
-func AutoAuthenticateHanlder(res http.ResponseWriter, req *http.Request) {
-	// This tries to get the user without re-authenticating
-	if gothUser, err := gothic.CompleteUserAuth(res, req); err == nil {
-		log.Println("If executed")
-		t, _ := template.ParseFiles("templates/success.html")
-		t.Execute(res, gothUser)
-	} else {
-		log.Println("Else executed")
-		gothic.BeginAuthHandler(res, req)
-	}
-}
-
-// RootHandler is ...
-func RootHandler(res http.ResponseWriter, req *http.Request) {
-	m := make(map[string]string)
-	m["twitter"] = "Twitter"
-
-	providerIndex := &ProviderIndex{
-		Provider:    "twitter",
-		ProviderMap: m,
-	}
-	t, _ := template.ParseFiles("templates/index.html")
-	t.Execute(res, providerIndex)
+// ProviderIndex is ...
+type ProviderIndex struct {
+	Provider    string
+	ProviderMap map[string]string
 }
 
 func main() {
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	fmt.Println("Connected to MongoDB!")
+
 	goth.UseProviders(
 		twitter.New("78KQ0Abr9LVvMRoJhF3952pqS", "VjHpiYvFWS6ntGDGmIHR8aIRRLE4kxHDKhj0oDF9bNBU4rF983", "http://127.0.0.1:3000/auth/twitter/callback"),
 	)
@@ -66,20 +56,69 @@ func main() {
 
 	p := pat.New()
 
-	p.Get("/auth/{provider}/callback", CallbackHandler)
+	p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
 
-	p.Get("/logout/{provider}", LogoutHandler)
+		user, err := gothic.CompleteUserAuth(res, req)
+		if err != nil {
+			fmt.Fprintln(res, err)
+			return
+		}
+		t, _ := template.ParseFiles("templates/success.html")
+		t.Execute(res, user)
 
-	p.Get("/auth/{provider}", AutoAuthenticateHanlder)
+		// defer cursor.Close(ctx)
+		// for cursor.Next(ctx) {
+		// 	var userDoc bson.M
+		// 	if err = cursor.Decode(&userDoc); err != nil {
+		// 		log.Fatal(err)
+		// 	}
+		// 	if userDoc["userid"] == "ssssssss" {
+		// 		fmt.Println("It's here")
+		// 	}
+		// }
 
-	p.Get("/", RootHandler)
+		myuserdataCollection := client.Database("testdb").Collection("myuserdata")
+
+		//Insert One document
+		user1 := UserData{user.UserID, user.Email, user.Name}
+		insertResult, err := myuserdataCollection.InsertOne(context.TODO(), user1)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+
+	})
+
+	p.Get("/logout/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		gothic.Logout(res, req)
+		res.Header().Set("Location", "/")
+		res.WriteHeader(http.StatusTemporaryRedirect)
+	})
+
+	p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		// This tries to get the user without re-authenticating
+		if gothUser, err := gothic.CompleteUserAuth(res, req); err == nil {
+			log.Println("If executed")
+			t, _ := template.ParseFiles("templates/success.html")
+			t.Execute(res, gothUser)
+		} else {
+			log.Println("Else executed")
+			gothic.BeginAuthHandler(res, req)
+		}
+	})
+
+	p.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		m := make(map[string]string)
+		m["twitter"] = "Twitter"
+
+		providerIndex := &ProviderIndex{
+			Provider:    "twitter",
+			ProviderMap: m,
+		}
+		t, _ := template.ParseFiles("templates/index.html")
+		t.Execute(res, providerIndex)
+	})
 
 	log.Println("listening on localhost:3000")
 	log.Fatal(http.ListenAndServe(":3000", p))
-}
-
-// ProviderIndex is ...
-type ProviderIndex struct {
-	Provider    string
-	ProviderMap map[string]string
 }
---
+++ test/test1.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// UserData is ...
+type UserData struct {
+	UserID string
+	Email  string
+	Name   string
+}
+
+func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	fmt.Println("Connected to MongoDB!")
+
+	tempDB := client.Database("testdb")
+	testCollection := tempDB.Collection("myuserdata")
+
+	cursor, err := testCollection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Println("Not found")
+		log.Fatal(err)
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var userDoc bson.M
+		if err = cursor.Decode(&userDoc); err != nil {
+			log.Fatal(err)
+		}
+		if userDoc["name"] == "sksalmahaider" {
+			fmt.Println("It's here")
+		}
+	}
+
+	// var episodesFiltered []bson.M
+	// if err = filterCursor.All(ctx, &episodesFiltered); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// for c, x := range episodesFiltered {
+	// 	fmt.Println(c, x["name"])
+	// }
+
+	myuserdataCollection := client.Database("testdb").Collection("myuserdata")
+
+	//Insert One document
+	user1 := UserData{"ssssssss", "hhhhhh@gmail.com", "rrrrrr"}
+	insertResult, err := myuserdataCollection.InsertOne(context.TODO(), user1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+
+}
