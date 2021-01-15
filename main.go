d8726af174eb869999f449ae114481bb0d1d5313
dexter1918 <sksalmanhaider@outlook.com>
2021-01-15 21:56:24 +0530

    Final1

--- main.go
+++ main.go
@@ -40,6 +40,18 @@ func main() {
 	)
 	gothic.Store = sessions.NewCookieStore([]byte("VjHpiYvFWS6ntGDGmIHR8aIRRLE4kxHDKhj0oDF9bNBU4rF983"))
 
+	// key := "VjHpiYvFWS6ntGDGmIHR8aIRRLE4kxHDKhj0oDF9bNBU4rF983" // Replace with your SESSION_SECRET or similar
+	// maxAge := 86400 * 30                                        // 30 days
+	// isProd := false                                             // Set to true when serving over https
+
+	// store := sessions.NewCookieStore([]byte(key))
+	// store.MaxAge(maxAge)
+	// store.Options.Path = "/"
+	// store.Options.HttpOnly = true // HttpOnly should always be enabled
+	// store.Options.Secure = isProd
+
+	// gothic.Store = store
+
 	p := pat.New()
 
 	p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
